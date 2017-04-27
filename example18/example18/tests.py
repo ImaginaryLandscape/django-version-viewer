760c7f37bf08c1dffb8704d38bbd5657a521ca1b
petercat12 <Pwensel@gmail.com>
2017-04-27 10:31:39 -0500

    add tests for the view.

---
+++ example18/example18/tests.py
@@ -0,0 +1,66 @@
+import mock
+import unittest
+import json
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.test import Client
+
+
+class TestVersionViewer(unittest.TestCase):
+
+    url_django_version_viewer = reverse('django_version_viewer')
+
+    mock_data = [
+         {"key": "appdirs", "version": "1.4.3"},
+         {"key": "django", "version": "1.8.18"},
+         {"key": "six", "version": "1.10.0"}
+    ]
+
+    def mocked_pip_get_installed_distributions(self, *args, **kwargs):
+        class MockPipObject:
+            def __init__(self, key, version):
+                self.key = key
+                self.version = version
+
+        class MockResponse:
+            def __init__(self, data):
+                self.packages = []
+                for _dict in data:
+                    mocked_obj = MockPipObject(_dict['key'], _dict['version'])
+                    self.packages.append(mocked_obj)
+
+            def get_installed_distributions(self):
+                return self.packages
+
+        return MockResponse(self.mock_data).get_installed_distributions()
+
+    def setUp(self):
+        self.admin = User.objects.create_superuser(
+            email="adminmail@mail.com",
+            username="admin_user",
+            password="password",
+        )
+        self.user = User.objects.create(
+            email="user@usermail.com",
+            username="regular_user",
+            password="password"
+        )
+
+    def tearDown(self):
+        self.admin.delete()
+        self.user.delete()
+
+    def test_django_version_viewer_view_admin(self):
+        client = Client()
+        client.login(username=self.admin.username, password="password")
+        with mock.patch('pip.get_installed_distributions', side_effect=self.mocked_pip_get_installed_distributions):
+            response = client.get(self.url_django_version_viewer)
+            json_response = json.loads(response.content)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(json_response), 3)
+
+    def test_django_version_viewer_view_user(self):
+        client = Client()
+        client.login(username=self.user.username, password="password")
+        response = client.get(self.url_django_version_viewer)
+        self.assertEqual(response.status_code, 403)
