e19fca122d84137a7348c6474226c60971908c49
David Bertrand <dbertrand@imagescape.com>
2017-05-05 20:03:37 +0000

    Updated readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django_version_viewer',
-    version='0.0.1a8',
+    version='0.0.1a9',
     packages=['django_version_viewer'],
     install_requires=(
     ),
