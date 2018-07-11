a3bf0d78963b5c6dfa9cebc593a5f5f56e1a0608
Rodica Trifan <rodica@imagescape.com>
2018-07-11 16:22:46 -0500

    Switch from using pip.get_installed_distributions to pkg_resources.working_set

--- django_version_viewer/pip_viewer.py
+++ django_version_viewer/pip_viewer.py
@@ -1,5 +1,5 @@
-import pip
 from operator import itemgetter
+import pkg_resources
 
 
 def list_package_versions():
@@ -8,7 +8,7 @@ def list_package_versions():
 
     {'package_name': "django", 'package_version': "1.8.18"}
     """
-    installed_packages = pip.get_installed_distributions()
+    installed_packages = pkg_resources.WorkingSet()
     results = [{"package_name": i.key, "package_version": i.version} for i in installed_packages]
     return sorted(results, key=itemgetter('package_name'))
 
@@ -17,7 +17,7 @@ def get_pip_packages_csv(writer):
     """
     Takes a csv writer and writes installed pip packages to it.
     """
-    installed_packages = pip.get_installed_distributions()
+    installed_packages = pkg_resources.WorkingSet()
     for i in installed_packages:
         writer.writerow([i.key, i.version])
     return writer
