bb04183cc314e815fb3b747944459f1aac0b69ca
alexkosotukhin <akosotukhin@gmail.com>
2023-05-30 09:08:45 +0300

    Django 4+ support introduced

--- django_version_viewer/urls.py
+++ django_version_viewer/urls.py
@@ -1,14 +1,9 @@
-try:
-    # django 1.6+
-    from django.conf.urls import url
-except ImportError:
-    # django <1.6
-    from django.conf.urls.defaults import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.DjangoVersionViewer.as_view(), name='django_version_viewer'),
-    url(r'^csv/$', views.DjangoVersionViewerCSV.as_view(), name='django_version_viewer_csv'),
-    url(r'^toolbar/$', views.DjangoVersionViewerToolBar.as_view(), name='django_version_viewer_toolbar'),  # noqa
+    path('', views.DjangoVersionViewer.as_view(), name='django_version_viewer'),
+    path('csv/', views.DjangoVersionViewerCSV.as_view(), name='django_version_viewer_csv'),
+    path('toolbar/', views.DjangoVersionViewerToolBar.as_view(), name='django_version_viewer_toolbar'),  # noqa
 ]
